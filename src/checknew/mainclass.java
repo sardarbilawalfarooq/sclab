5b4990f4451dcebf626724dab5210c55fb1b61d0
DeeByte Computers <DeeByte Computers@172.16.105.221>
2016-05-30 17:23:06 +0500

    how are you friends.................

---
+++ src/checknew/mainclass.java
@@ -0,0 +1,19 @@
+package checknew;
+
+import java.util.Scanner;
+
+public class mainclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ String s1="asd";
+ String s2="bilo";
+   //Scanner sc= new Scanner(System.in);
+		 if (s1==s2)
+			 System.out.println("text compared0");
+		 else
+			 System.out.println("text not compared0");
+			 
+	}
+
+}
